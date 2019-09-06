e3f3dd8443acdeab6b30bb0b78b4bf7f26c2d826
Stef van Dijk <stef@vandijkstef.nl>
2019-09-06 09:55:38 +0200

    I made a clock!

--- client/src/Clock/Clock.tsx
+++ client/src/Clock/Clock.tsx
@@ -6,11 +6,29 @@ interface ClockProps {
 
 export default class Clock extends React.Component<{}, ClockProps> {
 	
+	private timerID?: any; // NodeJS.Timeout should be here, but then clearInterval isn't accepting it. Not something I want to work out today
+
 	constructor(props: {}) {
 		super(props);
 		this.state = {
 			date: new Date()
 		}
+	}
+
+	componentDidMount() {
+		this.timerID = setInterval(() => {
+			this.tick()
+		}, 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	tick() {
+		this.setState({
+			date: new Date()
+		});
 	}
 
 	render() {
