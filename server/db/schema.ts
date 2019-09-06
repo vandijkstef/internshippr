e8976085a933b5202b96a8cf92c2aa44313dc1df
Stef van Dijk <stef@vandijkstef.nl>
2019-09-06 16:49:41 +0200

    some DB efforts

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,4 +1,5 @@
 import * as graphql from 'graphql';
+import { Sequelize, Model, DataTypes } from 'sequelize';
 
 // Base types
 const {
@@ -6,23 +7,124 @@ const {
 	GraphQLSchema,
 } = graphql;
 
+// Data types
 const {
 	GraphQLString,
 	GraphQLID,
 } = graphql;
 
+// Base
+interface BaseData {
+	id: number | object;
+}
+const BaseFields = {
+	id: {
+		type: GraphQLID
+	}
+}
+
+// User
+interface UserData extends BaseData {
+	name: string | object;
+	email: string | object;
+	password: string | object;
+}
+const UserFields: UserData = Object.assign({}, BaseFields, {
+	name: {
+		type: GraphQLString
+	},
+	email: {
+		type: GraphQLString,
+	},
+	password: {
+		type: GraphQLString,
+		resolve: (user) => null,
+	}
+});
 const UserType = new GraphQLObjectType({
 	name: 'User',
-	fields: () => ({
-		id: {
-			type: GraphQLID
-		},
-		name: {
-			type: GraphQLString
-		}
-	})
+	fields: () => (UserFields as any)
 });
 
+// Log
+interface LogData {
+	message: object;
+}
+const LogFields = Object.assign({}, BaseFields, {
+	message: {
+		type: GraphQLString
+	}
+});
+const LogType = new GraphQLObjectType({
+	name: 'Log',
+	fields: () => (LogFields)
+});
+
+// DB Setup
+const sequelize = new Sequelize('postgres', 'postgres', 'docker', {
+	dialect: 'postgres',
+	define: {
+		timestamps: true
+	}
+});
+
+sequelize.authenticate()
+	.then(() => {
+		console.log('DB Connection: OK');
+	})
+	.catch((err: Error) => {
+		console.error(err);
+		console.log('DB Connection: Failed');
+		process.exit(1);
+	});
+
+// Setup DB Tables
+class User extends Model {};
+const UserModel = {
+	fields: {
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	},
+	settings: {
+		sequelize,
+		modelName: 'user'
+	},
+}
+User.init(UserModel.fields, UserModel.settings);
+
+// Mutations
+const Mutation = new GraphQLObjectType<undefined, undefined, any>({
+	name: 'Mutation',
+	fields: {
+		addUser: {
+			type: UserType,
+			args: UserFields as any,
+			resolve(parent: any, args: UserData) {
+				// TODO: Save to DB
+			}
+		},
+		addLog: {
+			type: LogType,
+			args: LogFields as any,
+			resolve(parent: any, args: LogData) {
+				// TODO: Save to db
+				console.log(args.message);
+			}
+		}
+	}
+});
+
+// Root
 const RootQuery = new GraphQLObjectType({
 	name: 'RootQuery',
 	fields: {
@@ -42,5 +144,6 @@ const RootQuery = new GraphQLObjectType({
 });
 
 module.exports = new GraphQLSchema({
-	query: RootQuery
+	query: RootQuery,
+	mutation: Mutation,
 });
