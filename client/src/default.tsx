1dd1dfbe275d8520df70e25d8a166a7a787fc173
Stef van Dijk <stef@vandijkstef.nl>
2019-09-06 14:01:55 +0200

    React comp. default template

---
+++ client/src/default.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export interface Props {
+	// No props
+}
+
+export interface State {
+	// No state
+}
+
+export default class DEFAULT extends React.Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+
+		this.state = {
+			// Set default states
+		}
+	}
+
+	render() {
+		return (
+			// Render here
+		)
+	}
+}
